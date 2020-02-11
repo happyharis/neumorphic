0652273b4cfaaf027a3bca1b9f28dd7d77704b51
Happy Haris <muhamad_haris@hotmail.com>
2020-02-11 10:47:08 +0800

    ✨ Add digital colon

---
+++ lib/timer/digital_font/digital_colon.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class DigitalColon extends StatelessWidget {
+  final double height;
+  final Color color;
+
+  DigitalColon({Key key, @required this.height, @required this.color})
+      : assert(height != null),
+        assert(color != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      size: new Size(height / 2.0, height),
+      painter: new _DigitalColonPainter(height, color),
+    );
+  }
+}
+
+class _DigitalColonPainter extends CustomPainter {
+  final double height;
+  final Color color;
+
+  _DigitalColonPainter(this.height, this.color);
+
+  @override
+  bool shouldRepaint(_DigitalColonPainter oldDelegate) {
+    return height != oldDelegate.height || color != oldDelegate.color;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double width = height / 2;
+    final double thickness = width / 5;
+
+    final Paint paint = new Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    // Top dot
+    canvas.drawRect(
+        new Rect.fromLTWH(
+          width / 2 - thickness / 2,
+          height / 3 - thickness / 2,
+          thickness,
+          thickness,
+        ),
+        paint);
+    // Bottom dot
+    canvas.drawRect(
+        new Rect.fromLTWH(
+          width / 2 - thickness / 2,
+          height * 2 / 3 - thickness / 2,
+          thickness,
+          thickness,
+        ),
+        paint);
+  }
+}
