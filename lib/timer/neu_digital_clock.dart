20e7001af8dd87140af5cc53b21876ebd561b006
Happy Haris <muhamad_haris@hotmail.com>
2020-02-11 09:39:36 +0800

    💄 Add digitial clock background

--- lib/timer/neu_digital_clock.dart
+++ lib/timer/neu_digital_clock.dart
@@ -9,7 +9,41 @@ class NeuDigitalClock extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 145,
-      color: Colors.green,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(217, 230, 243, 1),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 15,
+            offset: Offset(-5, -5),
+            color: Colors.white,
+          ),
+          BoxShadow(
+            blurRadius: 15,
+            offset: Offset(10.5, 10.5),
+            color: Color.fromRGBO(214, 223, 230, 1),
+          )
+        ],
+      ),
+      child: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) => Container(
+            height: constraints.maxHeight * 0.87,
+            width: constraints.maxWidth * 0.95,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(203, 211, 196, 1),
+                Color.fromRGBO(176, 188, 163, 1)
+              ]),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Color.fromRGBO(168, 168, 168, 1),
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
